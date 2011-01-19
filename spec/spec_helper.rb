fb4f3940d87d08f51b93099504cf99fe38271326
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-19 16:14:28 +0900

    Add helper constants.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'gotaku'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+ASSETS_DIR  = File.join File.dirname(__FILE__), 'files'
+GOTAKU_FILE = File.join ASSETS_DIR, 'test.5tq'
 
 RSpec.configure do |config|
-  
 end
